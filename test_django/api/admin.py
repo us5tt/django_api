8e149e92980705388120e990b26af65708acf05d
Serg <serg@MacBook-Pro-Serg.local>
2021-10-10 23:56:45 +0300

    API_Django

---
+++ test_django/api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Pars
+
+
+admin.site.register(Pars)
---
+++ test_django/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ParsView
+
+
+app_name = "items"
+
+urlpatterns = [
+    path('items/', ParsView.as_view()),
+    path('items/<int:pk>', ParsView.as_view())
+]
