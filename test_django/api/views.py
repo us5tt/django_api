79c0e85bc5ba8a815cf41ea816f6a4f96d1d55d0
serg <serg.manko80@gmail.com>
2021-10-11 23:13:40 +0300

    GET GET_id POST PUT DELITE

--- test_django/api/views.py
+++ test_django/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import get_object_or_404
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
 from django.http import HttpResponse
 
@@ -11,8 +12,16 @@ def index(request):
     return HttpResponse("Головна сторінка в розробці")
 
 
-class ParsView(CreateAPIView, ListAPIView):
+class ParsView(ListCreateAPIView):
     queryset = Pars.objects.all()
     serializer_class = ParsSerializer
 
 
+class SingleParsView(RetrieveUpdateAPIView):
+    queryset = Pars.objects.all()
+    serializer_class = ParsSerializer
+
+
+class SingleParsView(RetrieveUpdateDestroyAPIView):
+    queryset = Pars.objects.all()
+    serializer_class = ParsSerializer
