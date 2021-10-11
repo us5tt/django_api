770a5cae34ef146fe7505950df74a967caaee54a
serg <serg.manko80@gmail.com>
2021-10-11 21:47:31 +0300

    new metod GenericAPIView GET POST

--- test_django/api/serializers.py
+++ test_django/api/serializers.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
 from .models import Pars
 
+
+class ParsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pars
+        fields = ('title', 'usd_price', 'city', 'description', 'id')
+
+
--- test_django/test_django/urls.py
+++ test_django/test_django/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api import views
+
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
 ]
