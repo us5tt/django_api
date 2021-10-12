3ae1f87b9cf1ae59e313befd761be842662080f9
us5tt <58837161+us5tt@users.noreply.github.com>
2021-10-12 23:39:48 +0300

    Update serializers.py

--- test_django/api/serializers.py
+++ test_django/api/serializers.py
@@ -2,11 +2,8 @@ from rest_framework import serializers
 from .models import Pars
 
 
-
-class ParsSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=120)
-    usd_price = serializers.CharField()
-    city = serializers.CharField()
-    description = serializers.CharField()
-    id = serializers.IntegerField()
+class ParsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pars
+        fields = ('title', 'usd_price', 'city', 'description', 'id')
 
