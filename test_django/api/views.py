c9e6341c25387202188904f1ad5755c6f1761951
us5tt <58837161+us5tt@users.noreply.github.com>
2021-10-12 23:38:38 +0300

    Update views.py

--- test_django/api/views.py
+++ test_django/api/views.py
@@ -1,38 +1,22 @@
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+
+
+from django.http import HttpResponse
 
 from .models import Pars
 from .serializers import ParsSerializer
 
 
-class ParsView(APIView):
-    def get(self, request):
-        items = Pars.objects.all()
-        serializer = ParsSerializer(items, many=True)
-        return Response({"items": serializer.data})
+def index(request):
+    return HttpResponse("Головна сторінка в розробці")
 
-    def post(self, request):
-        items = request.data.get('items')
-        serializer = ParsSerializer(data=items)
-        if serializer.is_valid(raise_exception=True):
-            items_saved = serializer.save()
-        return Response({"success": "Items '{}' created successfully".format(items_saved.title)})
 
-    def put(self, request, pk):
-        saved_items = get_object_or_404(Pars.objects.all(), pk=pk)
-        data = request.data.get('items')
-        serializer = ParsSerializer(instance=saved_items, data=data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            items_saved = serializer.save()
-        return Response({
-            "success": "Items '{}' updated successfully".format(items_saved.title)
-        })
+class ParsView(ListCreateAPIView):
+    queryset = Pars.objects.all()
+    serializer_class = ParsSerializer
 
-    def delete(self, request, pk):
-        article = get_object_or_404(Pars.objects.all(), pk=pk)
-        article.delete()
-        return Response({
-            "message": "Items with id `{}` has been deleted.".format(pk)
-        }, status=204)
+
+class SingleParsView(RetrieveUpdateAPIView):
+    queryset = Pars.objects.all()
+    serializer_class = ParsSerializer
 
